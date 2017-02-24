10a1dd020dc27e28302a545071dfefacf68f6af6
Kash0321 <castillo.sergio@gmail.com>
2017-02-24 14:35:17 +0100

    Ejemplo inicial

---
+++ src/heroDetail.js
@@ -0,0 +1,31 @@
+(function(angular) {
+  'use strict';
+
+function HeroDetailController($element) {
+    var ctrl = this;
+
+    ctrl.$onInit = function() {
+        console.log("Hola onInit");
+        console.log($element);
+    }
+
+    ctrl.$postLink = function() {
+        console.log("Hola postLink");
+        console.log($element);
+        $($element).children(":input").addClass("red-back");
+        $($element).children(":input").css("background-color", "blue");
+        $($element).children(":input").css("color", "white");
+        $($element).children(":input").attr("ng-disabled", "true");
+    }
+}
+
+angular
+    .module('heroApp')
+    .component('heroDetail', {
+        templateUrl: './heroDetail.html',
+        controller: HeroDetailController,
+        bindings: {
+            hero: '='
+        }
+    });
+})(window.angular);
---
+++ src/index.js
@@ -0,0 +1,10 @@
+(function(angular) {
+  'use strict';
+angular
+    .module('heroApp', [])
+    .controller('MainCtrl', function MainCtrl() {
+        this.hero = {
+            name: 'Spawn'
+        };
+    });
+})(window.angular);
