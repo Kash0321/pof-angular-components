d73d823f030908a5514fe41a75c966244e4eb668
Kash0321 <castillo.sergio@gmail.com>
2017-03-11 12:52:32 +0100

    conversión a base64 ok, falta implementar el flujo real

--- src/heroDetail.js
+++ src/heroDetail.js
@@ -15,63 +15,35 @@ function HeroDetailController($element) {
     function fileNameChanged(event) {
         var files = event.target.files; // FileList object
 
-        // files is a FileList of File objects. List some properties.
-        var output = [];
+        // Loop through the FileList and render image files as thumbnails.
         for (var i = 0, f; f = files[i]; i++) {
-            alert(escape(f.name) + f.type || 'n/a' + ') - ' +
-                    f.size + ' bytes, last modified: ' +
-                    f.lastModifiedDate.toLocaleDateString());
-        }
-        //document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-    };
 
-    ctrl.pushImage = function() {
-        //ctrl.hero.photo000 = ctrl.hero.photo001;
-        //ctrl.hero.photo000Type = ctrl.hero.photo001Type;
-
-        // Check for the various File API support.
-        if (window.File && window.FileReader && window.FileList && window.Blob) {
-            alert(ctrl.fileName);
-            toDataUrl('C:/Users/sergio.castillo/Desktop/Nikon-1-V3-sample-photo.jpg', function(dataUri) {
-                alert(dataUri);
-            }, 'png');
-        } else {
-            alert('The File APIs are not fully supported in this browser.');
-        }
-    };
-
-
-    function toDataUrl(url, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.onload = function() {
-            var reader = new FileReader();
-            reader.onloadend = function() {
-                callback(reader.result);
+            // Only process image files.
+            if (!f.type.match('image.*')) {
+                continue;
             }
-            reader.readAsDataURL(xhr.response);
-        };
-        xhr.open('GET', url);
-        xhr.responseType = 'blob';
-        xhr.send();
-    }
 
-    function toDataUrl(src, callback, outputFormat) {
-        var img = new Image();
-        img.crossOrigin = 'Anonymous';
-        img.onload = function() {
-            var canvas = document.createElement('CANVAS');
-            var ctx = canvas.getContext('2d');
-            var dataURL;
-            canvas.height = this.height;
-            canvas.width = this.width;
-            ctx.drawImage(this, 0, 0);
-            dataURL = canvas.toDataURL(outputFormat);
-            callback(dataURL);
-        };
-        img.src = src;
-        if (img.complete || img.complete === undefined) {
-            img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
-            img.src = src;
+            var reader = new FileReader();
+
+            // Closure to capture the file information.
+            reader.onload = (function(theFile) {
+                return function(e) {
+
+                    var dataUrl = e.target.result;
+
+                    alert(dataUrl);
+
+                    // Render thumbnail.
+                    // var span = document.createElement('span');
+                    // span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                    //                     '" title="', escape(theFile.name), '"/>'].join('');
+                    // document.getElementById('list').insertBefore(span, null);
+                };
+            })(f);
+
+            // Read in the image file as a data URL.
+            reader.readAsDataURL(f);
+
         }
     }
 
